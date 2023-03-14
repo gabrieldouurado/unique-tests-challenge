01e61a2bd5317ff58ff6c12363b13b4fe88ed8d4
João Gabriel Dourado <joao.dourado96@gmail.com>
2023-03-14 15:47:57 -0300

    Create useCase tests

--- src/modules/statements/useCases/createStatement/CreateStatementUseCase.spec.ts
+++ src/modules/statements/useCases/createStatement/CreateStatementUseCase.spec.ts
@@ -12,6 +12,7 @@ let createStatementUseCase: CreateStatementUseCase
 enum OperationType {
   DEPOSIT = 'deposit',
   WITHDRAW = 'withdraw',
+  TRANSFER = 'transfer'
 }
 
 describe("Create Statement", () => {
@@ -66,6 +67,93 @@ describe("Create Statement", () => {
       await createStatementUseCase.execute({
         user_id,
         type: OperationType.WITHDRAW,
+        amount: 1000,
+        description: "Supermarket"
+      })
+    }).rejects.toBeInstanceOf(CreateStatementError.InsufficientFunds)
+  })
+
+  it("Should be able to create a new transfer statement", async () => {
+    const user = await createUserUseCase.execute({
+      name: "user_name",
+      email: "user@email.com",
+      password: "user@123"
+    })
+
+    const userReceived = await createUserUseCase.execute({
+      name: "user_received",
+      email: "received@email.com",
+      password: "received@123"
+    })
+
+    const user_id: string = user.id!
+
+    await createStatementUseCase.execute({
+      user_id,
+      type: OperationType.DEPOSIT,
+      amount: 100,
+      description: "Deposit"
+    })
+
+    const statement = await createStatementUseCase.execute({
+      user_id,
+      user_received_id: userReceived.id,
+      type: OperationType.TRANSFER,
+      amount: 36,
+      description: "Transfer"
+    })
+
+    expect(statement).toHaveProperty("id")
+    expect(statement.user_id).toEqual(user_id)
+  })
+
+  it("Should not be able to create a transfer when received not found", async () => {
+    const user = await createUserUseCase.execute({
+      name: "user_name",
+      email: "user@email.com",
+      password: "user@123"
+    })
+
+    const user_id: string = user.id!
+
+    await createStatementUseCase.execute({
+      user_id,
+      type: OperationType.DEPOSIT,
+      amount: 100,
+      description: "Deposit"
+    })
+
+    expect(async () => {
+      await createStatementUseCase.execute({
+        user_id,
+        user_received_id: "random@uuid",
+        type: OperationType.TRANSFER,
+        amount: 36,
+        description: "Transfer"
+      })
+    }).rejects.toBeInstanceOf(CreateStatementError.ReceiverNotFound)
+  })
+
+  it("Should not be able to create transfer with balance lessthan amount", async () => {
+    const user = await createUserUseCase.execute({
+      name: "user_name",
+      email: "user@email.com",
+      password: "user@123"
+    })
+
+    const userReceived = await createUserUseCase.execute({
+      name: "user_received",
+      email: "received@email.com",
+      password: "received@123"
+    })
+
+    const user_id: string = user.id!
+
+    expect(async () => {
+      await createStatementUseCase.execute({
+        user_id,
+        user_received_id: userReceived.id,
+        type: OperationType.TRANSFER,
         amount: 1000,
         description: "Supermarket"
       })
